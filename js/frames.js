e3b3dc19d4df3897babdc694570e868e351af471
encuentros-gianuc2 <encuentrosgianuc2@gmail.com>
2024-08-17 20:09:13 -0400

    Add files via upload

---
+++ js/frames.js
@@ -0,0 +1,25 @@
+   
+function frames(variable){
+        document.getElementById("enframe").src="pages/home.html";
+        document.getElementById("enframe").height="550";
+    if (variable==1){
+        document.getElementById("enframe").src="pages/home.html";
+        document.getElementById("enframe").height="550";
+    }
+    else if (variable==2){
+        document.getElementById("enframe").src="pages/programa.html";
+        document.getElementById("enframe").height="1100";
+    } 
+    else if (variable==3){
+        document.getElementById("enframe").src="pages/postulacion.html";
+        document.getElementById("enframe").height="550";
+    }
+    else if (variable==4){
+        document.getElementById("enframe").src="pages/patrocinadores.html";
+        document.getElementById("enframe").height="550";
+    }
+    else if (variable==5){
+        document.getElementById("enframe").src="pages/galeria.html";  
+        document.getElementById("enframe").height="550";
+    }   
+} 
