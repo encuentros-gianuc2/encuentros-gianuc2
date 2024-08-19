da16016d060e357244ea236d1305459bb0d2735b
encuentros-gianuc2 <encuentrosgianuc2@gmail.com>
2024-08-18 22:43:24 -0400

    Update frames.js

--- js/frames.js
+++ js/frames.js
@@ -1,10 +1,10 @@
    
 function frames(variable){
         document.getElementById("enframe").src="pages/home.html";
-        document.getElementById("enframe").height="600";
+        document.getElementById("enframe").height="620";
     if (variable==1){
         document.getElementById("enframe").src="pages/home.html";
-        document.getElementById("enframe").height="600";
+        document.getElementById("enframe").height="620";
     }
     else if (variable==2){
         document.getElementById("enframe").src="pages/programa.html";
@@ -12,7 +12,7 @@ function frames(variable){
     } 
     else if (variable==3){
         document.getElementById("enframe").src="pages/postulacion.html";
-        document.getElementById("enframe").height="550";
+        document.getElementById("enframe").height="1000";
     }
     else if (variable==4){
         document.getElementById("enframe").src="pages/patrocinadores.html";
